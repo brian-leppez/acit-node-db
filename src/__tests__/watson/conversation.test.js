f6b4823265156f355c1b255bd085e02d5d9d246c
Tom Jacobs <thomas.i.jacobs@accenture.com>
2017-11-21 15:25:57 -0600

    Added describe tag

--- src/__tests__/watson/conversation.test.js
+++ src/__tests__/watson/conversation.test.js
@@ -5,5 +5,6 @@ import Conversation from '../../../src/lib/watson/conversation';
 
 const conversation = new Conversation();
 
-test('confirm we get a response from Watson', () =>
-  expect(conversation.message('Hello')).resolves.toBeDefined());
+describe('smoke test', () =>
+  test('confirm we get a response from Watson', () =>
+    expect(conversation.message('Hello')).resolves.toBeDefined()));
