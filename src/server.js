bf4d824db2cb6c8f81c14a4a6f2a0dd52557c3ed
Brian Leppez <brian.leppez@accenture.com>
2017-11-22 11:56:53 -0700

    App now deployed, now reverting unnecessary changes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 /* @flow */
 /* eslint-disable no-console, no-shadow */
 
-import cfenv from 'cfenv';
+// import cfenv from 'cfenv';
 import app from './app';
-// import config from './config';
+import config from './config';
 
-const appEnv = cfenv.getAppEnv();
+// const appEnv = cfenv.getAppEnv();
 
 // Launch Node.js server
-const server = app.listen(appEnv.port, appEnv.bind, () => {
+const server = app.listen(config.port, config.host, () => {
   console.log(
-    `Node.js server is listening on http://${appEnv.bind}:${appEnv.port}/`,
+    `Node.js server is listening on http://${config.host}:${config.port}/`,
   );
 });
 
