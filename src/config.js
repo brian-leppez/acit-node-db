bf04f888b6a832545592f121906cb1ead107bcaf
Brian Leppez <brian.leppez@accenture.com>
2017-11-21 22:14:59 -0700

    Changed env variable name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Node.js app
   port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
+  host: process.env.VCAP_APP_HOST || 'localhost',
 
   watson: {
     conversation: {
